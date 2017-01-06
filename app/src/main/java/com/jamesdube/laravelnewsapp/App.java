5d785e31b0febc9b85de345f3a8687a3c50c88a7
James Dube <jamesddube@live.com>
2017-01-07 00:01:23 +0200

    create App class for singletons

---
+++ app/src/main/java/com/jamesdube/laravelnewsapp/App.java
@@ -0,0 +1,33 @@
+package com.jamesdube.laravelnewsapp;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Created by rick on 1/6/17.
+ */
+
+public class App extends Application {
+    private static App Instance;
+    public static String Tag = "REALM";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Instance = this;
+    }
+
+    public static App getInstance() {
+        if(Instance == null) {
+            Log.d(Tag,"app init");
+            Instance = new App();
+        }
+        return Instance;
+    }
+
+    public static Context getAppContext() {
+        return getInstance().getApplicationContext();
+    }
+
+}
