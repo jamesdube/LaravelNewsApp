d1adc1e9873b2f963d9fe37e3323e2d8bea0a8b3
James Dube <jamesddube@live.com>
2017-01-08 01:22:44 +0200

    add Request helper

---
+++ app/src/main/java/com/jamesdube/laravelnewsapp/util/request.java
@@ -0,0 +1,11 @@
+package com.jamesdube.laravelnewsapp.util;
+
+/**
+ * Created by rick on 1/7/17.
+ */
+
+public class request {
+    public static class URL {
+        public static String LARAVEL_FEED_URL = "http://10.0.2.2:9000";
+    }
+}
