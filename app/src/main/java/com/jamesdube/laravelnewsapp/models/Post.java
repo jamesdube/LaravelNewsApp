aa0933d3766e3c23c3d9be4120fac0db153d36bb
James Dube <jamesddube@live.com>
2017-01-12 12:18:51 +0200

    add more post properties

--- app/src/main/java/com/jamesdube/laravelnewsapp/models/Post.java
+++ app/src/main/java/com/jamesdube/laravelnewsapp/models/Post.java
@@ -7,6 +7,11 @@ import com.android.volley.toolbox.NetworkImageView;
 import com.google.gson.Gson;
 import com.jamesdube.laravelnewsapp.App;
 
+import org.json.JSONArray;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by rick on 1/6/17.
  */
@@ -27,6 +32,14 @@ public class Post {
 
     //The Author of the Post
     private String creator;
+
+    //The Description of the Post
+    private String description;
+
+    //The Published Date
+    private String pubDate;
+
+
 
 
     /**
@@ -87,5 +100,29 @@ public class Post {
 
     public static Post fromJson(String json) {
         return App.Gson().fromJson(json, Post.class);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public void extractImageUrl() {
+        Pattern pattern = Pattern.compile("<img src=\"(.*?)\">");
+        Matcher matcher = pattern.matcher(getDescription());
+        if(matcher.find()){
+            setCoverImage (matcher.group(1));
+        }
     }
 }
