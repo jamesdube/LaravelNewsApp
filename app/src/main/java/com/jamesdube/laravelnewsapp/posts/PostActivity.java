32dcd98b0f81ecebcf494fd4b73c1289b0bf1a80
James Dube <thelonesoldier2301@gmail.com>
2017-02-21 10:26:53 +0200

    general updates

--- app/src/main/java/com/jamesdube/laravelnewsapp/posts/PostActivity.java
+++ app/src/main/java/com/jamesdube/laravelnewsapp/posts/PostActivity.java
@@ -6,10 +6,13 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -26,11 +29,13 @@ import com.github.bkhezry.extrawebview.data.DataModelBuilder;
 import com.jamesdube.laravelnewsapp.App;
 import com.jamesdube.laravelnewsapp.MainActivity;
 import com.jamesdube.laravelnewsapp.R;
-import com.jamesdube.laravelnewsapp.SettingsActivity;
+import com.jamesdube.laravelnewsapp.Settings;
 import com.jamesdube.laravelnewsapp.adapters.PostAdapter;
 import com.jamesdube.laravelnewsapp.models.Post;
 import com.jamesdube.laravelnewsapp.models.PostRepository;
 import com.jamesdube.laravelnewsapp.sync.SyncAdapter;
+import com.jamesdube.laravelnewsapp.util.MenuTint;
+import com.jamesdube.laravelnewsapp.util.Prefs;
 import com.jamesdube.laravelnewsapp.util.Themes;
 
 import org.ocpsoft.prettytime.PrettyTime;
@@ -39,6 +44,7 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.TimeZone;
 
 import io.realm.Realm;
@@ -51,6 +57,14 @@ public class PostActivity extends AppCompatActivity {
     ImageView postImage;
     Post post;
     CoordinatorLayout coordinatorLayout;
+    private ShareActionProvider mShareActionProvider;
+    Toolbar toolbar;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.d(App.Tag,"on post create");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +97,9 @@ public class PostActivity extends AppCompatActivity {
         //load the content
         webView.loadDataWithBaseURL("file:///android_asset/", post.getDescription(), "text/html", "utf-8", null);
 
+        //setupFavIcon();
+        Log.d(App.Tag,"on create");
+
     }
 
     @NonNull
@@ -105,11 +122,12 @@ public class PostActivity extends AppCompatActivity {
 
 
     private void boot(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if( getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         }
 
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.postCoordinatorLayout);
@@ -124,7 +142,15 @@ public class PostActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_post, menu);
+        setupFavIcon();
+        Log.d(App.Tag,"on create options");
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(App.Tag,"on start");
     }
 
     @Override
@@ -134,12 +160,18 @@ public class PostActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(App.getAppContext(),SettingsActivity.class));
+            startActivity(new Intent(App.getAppContext(),Settings.class));
             return true;
         }else  if (id == R.id.action_favorite) {
            favourite(post);
+        }else  if (id == R.id.action_share) {
+            shareTextUrl();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -152,6 +184,7 @@ public class PostActivity extends AppCompatActivity {
                 if(!post.isFavourite()){
                     message = "Post removed from favourites";
                 }
+                setupFavIcon();
                 showSnackBar(message);
             }
 
@@ -160,37 +193,37 @@ public class PostActivity extends AppCompatActivity {
                 showSnackBar("An error occurred");
             }
         });
-        /*final Post copied = App.Realm().copyFromRealm(post);
-        App.Realm().executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                Log.d(App.Tag, "marking all posts as seen.... ");
-                Post postFav = realm.where(Post.class).equalTo("link",copied.getLink()).findFirst();
-                Boolean change = postFav.isFavourite();
-                if(postFav.isFavourite() == null){
-                    change = false;
-                }
-                    postFav.setFavourite(!change);
-                    Log.d(App.Tag, "post fav => " + String.valueOf(postFav.isFavourite()));
-                //fav.setFavourite(true);
-            }
-        }, new Realm.Transaction.OnSuccess() {
-            @Override
-            public void onSuccess() {
-                showSnackBar("Post added to favourites");
-            }
-        }, new Realm.Transaction.OnError() {
-            @Override
-            public void onError(Throwable error) {
-                showSnackBar("Error adding to favourites");
-                error.printStackTrace();
-            }
-        });
-
-        Log.d(App.Tag,"post favourite => " + String.valueOf(post.isFavourite()));*/
     }
 
     public void showSnackBar(String message) {
         Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void shareTextUrl() {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.setType("text/plain");
+
+        // Add data to the intent, the receiving app will decide
+        // what to do with it.
+        share.putExtra(Intent.EXTRA_SUBJECT, post.getTitle());
+        share.putExtra(Intent.EXTRA_TEXT, post.getLink() + " via Laravel News App .Get the application here (link)");
+
+        startActivity(Intent.createChooser(share, "Share link!"));
+    }
+
+    private void setupFavIcon(){
+
+        int favIcon = R.drawable.ic_favorite;
+        if(post.isFavourite() != null && post.isFavourite()){
+            //change the icon
+            favIcon = R.drawable.ic_favorite_true;
+        }
+        toolbar.getMenu().getItem(0).setIcon(favIcon);
+        int color;
+        color = getResources().getColor(R.color.white);
+        if(Objects.equals(Prefs.getTheme(), Prefs.THEME_LIGHT)){
+            color = getResources().getColor(R.color.textColorSecondary);
+        }
+        MenuTint.colorIcons(this,toolbar.getMenu(),color);
     }
 }
