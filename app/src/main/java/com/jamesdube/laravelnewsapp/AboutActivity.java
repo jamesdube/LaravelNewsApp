0f71cae94f8a419409508f68d6248cbf5e7eb2c2
James Dube <thelonesoldier2301@gmail.com>
2017-02-21 02:45:22 +0200

    add About Activity

---
+++ app/src/main/java/com/jamesdube/laravelnewsapp/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.jamesdube.laravelnewsapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import mehdi.sakout.aboutpage.AboutPage;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.mipmap.ic_launcher)
+                .setDescription(getResources().getString(R.string.activity_about_description))
+                //                .addItem(versionElement)
+                //              .addItem(adsElement)
+                .addGroup("Laravel News")
+                .addWebsite("http://laravel-news.com")
+                .addFacebook("laravelnews")
+                .addTwitter("laravelnews")
+                .addYoutube("UC_HmfSCvAl_JaqdwN5YSVsg")
+                .addInstagram("laravelnews")
+                .addGroup("Laravel News App")
+                .addEmail("jamesddube@live.com")
+                .addWebsite("http://jamesdube.com")
+                .addTwitter("jamesddube")
+                .addPlayStore("com.jamesdube.laravelnewsapp")
+                .addGitHub("jamesddube")
+                .addInstagram("jamesddube")
+                .create();
+        setContentView(aboutPage);
+    }
+}
